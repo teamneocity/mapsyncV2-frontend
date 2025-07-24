d47cd319a3f26ad8ea345536559f744f6e32aca4
DiogoBastos-03 <kekediodiogo@gmail.com>
2025-07-24 11:14:17 -0300

    feat : opção de logar apenas apertando enter

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -14,7 +14,6 @@ import { Button } from "@/components/ui/button";
 import logo from "../../assets/logo.png";
 import Cell from "@/assets/icons/Cell.svg?react";
 
-
 export function SignIn() {
   const { signIn } = useAuth();
   const [email, setEmail] = useState("");
@@ -28,13 +27,17 @@ export function SignIn() {
     setPassword(e.target.value);
   };
 
-  function handleSignIn() {
+  function handleSignIn(e) {
+    e.preventDefault(); // evita recarregar a página
     signIn({ email, password });
   }
 
   return (
     <div className="min-h-screen flex items-center justify-center font-manrope px-4 bg-[#EBEBEB]">
-      <div className="w-full max-w-[400px] min-h-[30rem] flex flex-col items-center justify-center gap-8 bg-[#EBEBEB] p-6 rounded-2xl text-center overflow-hidden">
+      <form
+        onSubmit={handleSignIn}
+        className="w-full max-w-[400px] min-h-[30rem] flex flex-col items-center justify-center gap-8 bg-[#EBEBEB] p-6 rounded-2xl text-center overflow-hidden"
+      >
         <div className="w-full text-left flex flex-col gap-2 max-w-[366px]">
           <p className="font-bold">Email</p>
           <Input
@@ -67,17 +70,14 @@ export function SignIn() {
         </div>
 
         <div className="w-full max-w-[366px] flex gap-2">
-          <Button
-            className="flex-1 h-[55px] rounded-[16px]"
-            onClick={handleSignIn}
-          >
+          <Button type="submit" className="flex-1 h-[55px] rounded-[16px]">
             Login
           </Button>
           <div className="h-[55px] w-[55px] bg-white flex items-center justify-center rounded-[16px]">
             <Cell className="w-6 h-6 text-[#4B4B62]" />
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
